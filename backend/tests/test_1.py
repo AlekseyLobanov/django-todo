7e02dff18434603868a4b9099040ee327e9fa4c2
Derinhelm <Derinhelm@yandex.ru>
2021-04-23 02:01:32 +0300

    Добавлен тест на простейшее создание ToDoList
    Для запуска docker-compose exec web python manage.py test

--- backend/tests/test_1.py
+++ backend/tests/test_1.py
@@ -11,17 +11,28 @@ from rest_framework.test import APITestCase
 from rest_framework.test import APIClient
 from django.contrib.auth.models import User
 from backend.api import router
+from collections import OrderedDict
 
 class EasyTest(APITestCase):
+
     def test_list(self):
         """
         The first test.
         """
         user = User.objects.create_user('test_user', 'test@test.com', 'test_password')
-
-        url = reverse('ToDoLists-list')#reverse('ToDoLists-list')
-        data = {}
+        url = reverse('ToDoLists-list')
         self.client.force_authenticate(user=user)
-        response = self.client.get(url, data, format='json')
+
+        response = self.client.get(url, {}, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response.data, [])
+        self.assertEqual(response.data, OrderedDict([('count', 0), ('next', None), ('previous', None), ('results', [])]))
+    
+        response = self.client.post(url, {"title": "ToDoList1"}, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.data['title'], "ToDoList1")
+
+        response = self.client.get(url, {}, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        self.assertEqual((response.data['count'], response.data['next'], response.data['previous'], \
+            response.data['results'][0]['title']), (1, None, None, "ToDoList1"))
