889acf6c4598bd7dff2966fbde45f38788c7f095
Derinhelm <Derinhelm@yandex.ru>
2021-04-26 11:46:01 +0300

    Добавлены тесты для ToDoList

--- backend/tests/test_1.py
+++ backend/tests/test_1.py
@@ -13,26 +13,69 @@ from django.contrib.auth.models import User
 from backend.api import router
 from collections import OrderedDict
 
-class EasyTest(APITestCase):
-
-    def test_list(self):
-        """
-        The first test.
-        """
-        user = User.objects.create_user('test_user', 'test@test.com', 'test_password')
-        url = reverse('ToDoLists-list')
-        self.client.force_authenticate(user=user)
-
-        response = self.client.get(url, {}, format='json')
+class EasyTest1(APITestCase):
+    def get_todo(self, expected_titles):
+        response = self.client.get(self.url, {}, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response.data, OrderedDict([('count', 0), ('next', None), ('previous', None), ('results', [])]))
-    
-        response = self.client.post(url, {"title": "ToDoList1"}, format='json')
+        real_titles = [data['title'] for data in response.data['results']]
+        self.assertEqual((response.data['count'], real_titles), \
+            (len(expected_titles), expected_titles))
+
+    def post_todo(self, to_do_title):
+        response = self.client.post(self.url, {"title": to_do_title}, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(response.data['title'], "ToDoList1")
+        self.assertEqual(response.data['title'], to_do_title)
+        return response.data['id']
 
-        response = self.client.get(url, {}, format='json')
+    def get_todo_by_id(self, id, expected_title):
+        url_with_id = reverse('ToDoLists-detail', args=(id,))
+        response = self.client.get(url_with_id, {id: id}, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['title'], expected_title)
 
-        self.assertEqual((response.data['count'], response.data['next'], response.data['previous'], \
-            response.data['results'][0]['title']), (1, None, None, "ToDoList1"))
+    def put_todo(self, id, new_title):
+        url_with_id = reverse('ToDoLists-detail', args=(id,))
+        response = self.client.put(url_with_id, {"title": new_title}, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['title'], new_title)
+
+    def patch_todo(self, id, new_title):
+        url_with_id = reverse('ToDoLists-detail', args=(id,))
+        response = self.client.patch(url_with_id, {"title": new_title}, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['title'], new_title)
+
+    def delete_todo(self, id):
+        url_with_id = reverse('ToDoLists-detail', args=(id,))
+        response = self.client.delete(url_with_id, {}, format='json')
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_todo(self):
+        """
+        Tests API for todo.
+
+        lists/: get, post
+        lists/{id}/: get, put (update), patch (partial_update), delete
+        """
+
+        user = User.objects.create_user('test_user', 'test@test.com', 'test_password')
+        self.client.force_authenticate(user=user)
+        self.url = reverse('ToDoLists-list')
+
+        self.get_todo([])
+        to_do_title_1, to_do_title_2 = "ToDoList1", "ToDoList2"
+        to_do_id1 = self.post_todo(to_do_title_1)
+        self.get_todo([to_do_title_1])
+        to_do_id2 = self.post_todo(to_do_title_2)
+        self.get_todo([to_do_title_1, to_do_title_2])
+
+        self.get_todo_by_id(to_do_id1, to_do_title_1)
+        self.get_todo_by_id(to_do_id2, to_do_title_2)
+
+        self.delete_todo(to_do_id2)
+        self.get_todo([to_do_title_1])
+
+        self.put_todo(to_do_id1, "ToDoList11")
+
+        self.patch_todo(to_do_id1, "ToDoList12")
+        
